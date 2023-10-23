2aa131c98961c831f1a96e8429c255f94145bfa4
mikkype <miguelcaceda@gmail.com>
2023-10-23 21:03:39 +0200

    modificacion de form y faq

--- js/form.js
+++ js/form.js
@@ -1,3 +1,35 @@
-function comentario(){
+document.addEventListener("DOMContentLoaded", function () {
+
+    var questions = document.querySelectorAll('.question');
+    var answers = document.querySelectorAll('.answer');
+
+    questions.forEach(function (question, index) {
+        question.addEventListener("click", function () {
+
+            answers.forEach(function (answer) {
+                answer.style.display = 'none';
+
+                answers[index].style.display = 'block';
+
+
+            });
+        });
+    });
+})
+
+
+
+
+function mostrarComentario() {
     alert("¡Gracias por dejarnos tu comentario! En breve podrás leerlo en esta sección.")
+    
+    var elemento = document.getElementById('elemento1').value;
+    var comentario = document.getElementById('mostrar').value;
+
+    if (elemento && comentario) {
+        var comentarioMostrado = document.getElementById('comentarioMostrado');
+        comentarioMostrado.innerHTML = '<p><b>' + elemento + '</b> dejo su comentario :</p> ' + comentario;
+    }else {
+        alert("Por favor, complete tanto el campo de nombre como el campo de comentario.");
+    }
 }
