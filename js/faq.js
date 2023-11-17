8b78da4064bf018aa89b9f236bf04e41b3ef9786
miguel <miguelcaceda@gmail.com>
2023-11-17 14:50:43 +0100

    modificacion

--- js/faq.js
+++ js/faq.js
@@ -4,40 +4,6 @@ function init() {
     console.log('DOM cargado')
 }
 
-function mostrarComentario() {
-    var nombre = document.getElementById('form4Example1').value;
-    var comentario = document.getElementById('form4Example3').value;
-
-    if (nombre && comentario) {
-        // Obtén la colección de elementos con la clase 'comentario'
-        var comentariosMostrados = document.getElementsByClassName('comentario');
-
-        // Itera sobre la colección y actualiza el contenido de cada elemento
-        for (var i = 0; i < comentariosMostrados.length; i++) {
-            comentariosMostrados[i].innerHTML =
-                `
-                <h3>${nombre} :</h3><p>dejó su comentario :</p>
-                <p>${comentario}</p>
-                `;
-        }
-    }
-}
-
-
-function mostrarComentario2() {
-    
-    var nombre = document.getElementById('form4Example1').value;
-    var comentario = document.getElementById('form4Example3').value;
-
-    if (nombre && comentario) {
-        var comentarioMostrado = document.getElementsByClassName('comentario');
-        comentarioMostrado.innerHTML =
-        ` 
-        <h3>${nombre} :</h3><p>dejó su comentario :</p>
-        <p>${comentario}</p>
-        `
-    }
-}
 
 function mostrarComentario1() {
     
@@ -53,6 +19,21 @@ function mostrarComentario1() {
         <p><b>${nombre}</b> dejó su comentario : ${comentario}</p>
         </div>
         </div>
+        `
+    }
+}
+
+function mostrarComentario2() {
+    
+    var nombre = document.getElementById('form4Example1').value;
+    var comentario = document.getElementById('form4Example3').value;
+
+    if (nombre && comentario) {
+        var comentarioMostrado = document.getElementsByClassName('comentario');
+        comentarioMostrado.innerHTML =
+        ` 
+        <h3>${nombre} :</h3><p>dejó su comentario :</p>
+        <p>${comentario}</p>
         `
     }
 }
