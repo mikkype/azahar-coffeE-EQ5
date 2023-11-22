fd37a3f94daa3973c8939b1393b9508c67205296
alexito1999 <72918060+alexito1999@users.noreply.github.com>
2023-11-22 18:11:41 +0100

    cartas y indice responsividad de nav

--- js/appGeneral.js
+++ js/appGeneral.js
@@ -1,19 +1,29 @@
 document.addEventListener("DOMContentLoaded", init);
+/* Variables globales de cada variable q contendra la base de datos de cada carta */
 let saladosDataCarta;
 let bebidasDataCarta;
 let dulcesDataCarta;
 let premiadosDataCarta;
+/* Funcion init que arranca los fecth y la funcion que alberga cada uno */
 
 function init() {
+                /* Traigo el json Bebidas*/
+
     fetch("/json/cartaBebidas.json")
+    /* Lo transformo en un objeto */
         .then(response => response.json())
+        /* Meto todos los datos del json en una variable data */
         .then(data => {
+            /* cambio la variable data por otra para que no se sobreescriban datos por tener varias del mismo nombre */
             bebidasDataCarta = data.bebidas;
+            /* Condicion que tenga esta clase para que se lanze la funcion */
             if (document.querySelector('.pagina-completaB')) {
                 infocompletaBebidas();
             }
         })
         .catch(error => console.error("Error al cargar el JSON: " + error));
+                /* Traigo el json Salados*/
+
     fetch("/json/cartaSalados.json")
         .then(response => response.json())
         .then(data => {
@@ -23,15 +33,21 @@ function init() {
             }
         })
         .catch(error => console.error("Error al cargar el JSON: " + error));
+                /* Traigo el json Premiados*/
+
     fetch("/json/cartaPremiados.json")
         .then(response => response.json())
         .then(data => {
             premiadosDataCarta = data.premiados;
             if (document.querySelector('.pagina-completaP')) {
                 infocompletaPremiados();
+                popo();
+
             }
         })
         .catch(error => console.error("Error al cargar el JSON: " + error));
+                /* Traigo el json Dulces*/
+
     fetch("/json/cartaDulces.json")
         .then(response => response.json())
         .then(data => {
@@ -43,8 +59,11 @@ function init() {
         .catch(error => console.error("Error al cargar el JSON: " + error));
 }
 
+                                    /* FUNCIONES */
+
+
 function infocompletaBebidas() {
-    // Obtener el primer elemento del JSON
+    // Cojer la base de datos correpondientes para hacer el foreach
     bebidasDataCarta.forEach(function (producto, index) {
         var nombreElement = document.getElementById("nombreb" + (index + 1));
         var imagenElement = document.getElementById("imageb" + (index + 1));
@@ -52,7 +71,7 @@ function infocompletaBebidas() {
         var precio = document.getElementById("preciob" + (index + 1));
 
         // Asignar la información del producto a los elementos HTML
-        precio.innerText = producto.Precio
+        precio.innerText ='Precio: '+ producto.Precio + '€';
         nombreElement.innerText = producto.Nombre;
         imagenElement.src = producto.image;
         imagenElement.alt = producto.Nombre + " Image";
@@ -60,7 +79,7 @@ function infocompletaBebidas() {
     });
 }
 function infocompletaSalados() {
-    // Obtener el primer elemento del JSON
+    // Cojer la base de datos correpondientes para hacer el foreach
     saladosDataCarta.forEach(function (producto, index) {
         var nombreElement = document.getElementById("nombres" + (index + 1));
         var imagenElement = document.getElementById("images" + (index + 1));
@@ -68,15 +87,16 @@ function infocompletaSalados() {
         var precio = document.getElementById("precios" + (index + 1));
 
         // Asignar la información del producto a los elementos HTML
-        precio.innerText = producto.Precio + '€';
         nombreElement.innerText = producto.Nombre;
         imagenElement.src = producto.image;
         imagenElement.alt = producto.Nombre + " Image";
-        descripcionElement.innerText = producto.Descripción;
+        descripcionElement.innerText = producto.Descripción;        
+        precio.innerText = 'Precio: '+producto.Precio + '€';
+
     });
 }
 function infocompletaDulces() {
-    // Obtener el primer elemento del JSON
+    // Cojer la base de datos correpondientes para hacer el foreach
     dulcesDataCarta.forEach(function (producto, index) {
         var nombreElement = document.getElementById("nombred" + (index + 1));
         var imagenElement = document.getElementById("imaged" + (index + 1));
@@ -84,7 +104,7 @@ function infocompletaDulces() {
         var precio = document.getElementById("preciod" + (index + 1));
 
         // Asignar la información del producto a los elementos HTML
-        precio.innerText = producto.Precio;
+        precio.innerText = 'Precio: '+producto.Precio + '€';
         nombreElement.innerText = producto.Nombre;
         imagenElement.src = producto.image;
         imagenElement.alt = producto.Nombre + " Image";
@@ -92,7 +112,7 @@ function infocompletaDulces() {
     });
 }
 function infocompletaPremiados() {
-    // Obtener el primer elemento del JSON
+    // Cojer la base de datos correpondientes para hacer el foreach
     premiadosDataCarta.forEach(function (producto, index) {
         var nombreElement = document.getElementById("nombrep" + (index + 1));
         var imagenElement = document.getElementById("imagep" + (index + 1));
@@ -100,11 +120,24 @@ function infocompletaPremiados() {
         var precio = document.getElementById("preciop" + (index + 1));
 
         // Asignar la información del producto a los elementos HTML
-        precio.innerText =producto.Precio
+        precio.innerText ='Precio: '+producto.Precio + '€';
         nombreElement.innerText = producto.Nombre;
         imagenElement.src = producto.image;
         imagenElement.alt = producto.Nombre + " Image";
         descripcionElement.innerText = producto.Descripción + ' ';            
 
+    });
+    
+}
+function popo() {
+    var ratingItems = document.querySelectorAll('.rating input');
+
+    ratingItems.forEach(function(item) {
+      var popover = new bootstrap.Popover(item, {
+        placement: 'top',
+        trigger: 'hover',
+        title: 'Calificación',
+        content: 'Haz clic para calificar'
+      });
     });
 }
--- js/appindice.js
+++ js/appindice.js
@@ -1,85 +1,132 @@
 document.addEventListener("DOMContentLoaded", init);
+/* Variables globales */
 let dulcesData;
 let saladosData;
 let bebidasData;
 let premiadosData;
 
+/* Funcion init */
 function init() {
+    /* Conversion a objeto del json Dulces y su funcion */
+
     fetch("/json/cartaDulces.json")
         .then(response => response.json())
         .then(data => {
             dulcesData = data.dulces;
-            // Lógica para mostrar o utilizar estos datos        
             infodulce();
         })
         .catch(error => console.error("Error al cargar el JSON de dulces: " + error));
+    /* Conversion a objeto del json Salados y su funcion */
 
     fetch("/json/cartaSalados.json")
         .then(response => response.json())
         .then(data => {
             saladosData = data.salados;
-            // Lógica para mostrar o utilizar estos datos
             infoSalados();
         })
         .catch(error => console.error("Error al cargar el JSON de salados: " + error));
+    /* Conversion a objeto del json Bebidas y su funcion */
+
     fetch("/json/cartaBebidas.json")
         .then(response => response.json())
         .then(data => {
             bebidasData = data.bebidas;
-            // Lógica para mostrar o utilizar estos datos
             infobebidas();
         })
         .catch(error => console.error("Error al cargar el JSON de salados: " + error));
+    /* Conversion a objeto del json Premiados y su funcion */
+
     fetch("/json/cartaPremiados.json")
         .then(response => response.json())
         .then(data => {
             premiadosData = data.premiados;
-            // Lógica para mostrar o utilizar estos datos
             infopremiados();
         })
         .catch(error => console.error("Error al cargar el JSON de salados: " + error));
 }
+
+/* FUNCIONES */
+
 /* Funcion para sacar los salados */
 
 function infoSalados() {
-    const listaSalados = document.getElementById("listaSalados");
-
+    const listaSalados1 = document.getElementById("listaSalados1");
+    const listaSalados2 = document.getElementById("listaSalados2");
+    listaSalados1.classList.add('listas');
+    listaSalados2.classList.add('listas2');
     saladosData.forEach((producto, index) => {
         const listItem = document.createElement('li');
-        listItem.textContent = `${producto.Nombre} - ${producto.Precio}€`;
-        listaSalados.appendChild(listItem);
+        listItem.textContent = producto.Nombre;
+        listaSalados1.appendChild(listItem);
+    });
+    saladosData.forEach((producto, index) => {
+        const listItem = document.createElement('li');
+        listaSalados2.style.listStyleType = 'none';
+
+        listItem.textContent = '€ ' + producto.Precio;
+        listaSalados2.appendChild(listItem);
     });
 }
+
+
 /* Funcion para sacar los dulces */
 
 function infodulce() {
-    const listaDulces = document.getElementById("listaDulces");
-
+    const listaDulces1 = document.getElementById("listaDulces1");
+    const listaDulces2 = document.getElementById("listaDulces2");
+    listaDulces1.classList.add('listas');
+    listaDulces2.classList.add('listas2');
     dulcesData.forEach((producto, index) => {
         const listItem = document.createElement('li');
-        listItem.textContent = `${producto.Nombre} - ${producto.Precio}€`;
-        listaDulces.appendChild(listItem);
+        listItem.textContent = producto.Nombre;
+        listaDulces1.appendChild(listItem);
+    });
+    dulcesData.forEach((producto, index) => {
+        const listItem = document.createElement('li');
+        listaDulces2.style.listStyleType = 'none';
+
+        listItem.textContent = '€ ' + producto.Precio;
+        listaDulces2.appendChild(listItem);
     });
 }
 /* Funcion para sacar los bebidas */
 
 function infobebidas() {
-    const listaDulces = document.getElementById("listaBebidas");
-
+    const listaBebidas1 = document.getElementById("listaBebidas1");
+    const listaBebidas2 = document.getElementById("listaBebidas2");
+    listaBebidas1.classList.add('listas');
+    listaBebidas2.classList.add('listas2');
     bebidasData.forEach((producto, index) => {
         const listItem = document.createElement('li');
-        listItem.textContent = `${producto.Nombre} - ${producto.Precio}€`;
-        listaDulces.appendChild(listItem);
+        listItem.textContent = producto.Nombre;
+        listaBebidas1.appendChild(listItem);
+    });
+    bebidasData.forEach((producto, index) => {
+        const listItem = document.createElement('li');
+        listaBebidas2.style.listStyleType = 'none';
+
+        listItem.textContent = '€ ' + producto.Precio;
+        listaBebidas2.appendChild(listItem);
     });
 }
 /* Funcion para sacar los premiados */
 
 function infopremiados() {
-    const listaDulces = document.getElementById("listaPremiados");
+    const listaPremiados1 = document.getElementById("listaPremiados1");
+    const listaPremiados2 = document.getElementById("listaPremiados2");
+    listaPremiados1.classList.add('listas');
+    listaPremiados2.classList.add('listas2');
 
     premiadosData.forEach((producto, index) => {
         const listItem = document.createElement('li');
-        listItem.textContent = `${producto.Nombre} - ${producto.Precio}€`;
-        listaDulces.appendChild(listItem);
+        listItem.textContent = producto.Nombre;
+        listaPremiados1.appendChild(listItem);
+    });
+    premiadosData.forEach((producto, index) => {
+        const listItem = document.createElement('li');
+        listaPremiados2.style.listStyleType = 'none';
+
+        listItem.textContent = '€ ' + producto.Precio;
+        listaPremiados2.appendChild(listItem);
     });
 }
